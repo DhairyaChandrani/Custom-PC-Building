2db3e320986279496a3bce061a395364d0067a1b
DhairyaChandrani <85739965+DhairyaChandrani@users.noreply.github.com>
2022-04-01 15:22:27 +0530

    Update connect_database.php

--- connect_database.php
+++ connect_database.php
@@ -3,10 +3,10 @@
 This file contains database connection configurationuser "root" and password ""
 */
 
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'wp_project');
+define('DB_SERVER', '	sql104.epizy.com');
+define('DB_USERNAME', '	epiz_31425047');
+define('DB_PASSWORD', '6yMtWlczEl561');
+define('DB_NAME', 'epiz_31425047_XXX');
 
 // $database - variable
 $database = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
